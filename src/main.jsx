ba8fae07104d332312a5458362b3186edc76b0d5
minatisleeping <minatt2002@gmail.com>
2024-07-29 15:23:20 +0700

    cache location..

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
-    domain="dev-gotr6iqppdutb6fm.us.auth0.com"
-    clientId="gGkD8KUti3TJCLscxr0mstoirXuMRkzt"
+    domain='dev-gotr6iqppdutb6fm.us.auth0.com'
+    clientId='gGkD8KUti3TJCLscxr0mstoirXuMRkzt'
     authorizationParams={{ redirect_uri: window.location.origin }}
+    cacheLocation='localstorage' // default value is 'memory'
   >
     <App />
   </Auth0Provider>
